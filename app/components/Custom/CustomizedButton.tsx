0604b7103ff726c8f4ca353a03936ea02005f8ec
Tanboon Tiengtut <tanboon.top3837@gmail.com>
2025-03-20 16:17:14 +0700

    feat: customized components

---
+++ app/components/Custom/CustomizedButton.tsx
@@ -0,0 +1,45 @@
+import Button, { ButtonProps } from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import React from "react";
+
+type ExtendedButtonProps = ButtonProps & {
+  testId?: string;
+  addStartIcon?: boolean;
+};
+
+const CustomizedButton = ({
+  sx,
+  variant,
+  title,
+  onClick,
+  startIcon,
+  type,
+  fullWidth,
+  color,
+  size,
+  disabled,
+  id,
+  testId,
+  addStartIcon,
+}: ExtendedButtonProps) => {
+  return (
+    <Button
+      id={id}
+      data-test-id={testId}
+      sx={sx}
+      type={type || "button"}
+      size={size || "small"}
+      variant={variant}
+      onClick={onClick}
+      startIcon={addStartIcon ? <AddIcon /> : startIcon}
+      fullWidth={fullWidth}
+      color={color}
+      disabled={disabled}
+      formNoValidate
+    >
+      {title}
+    </Button>
+  );
+};
+
+export default CustomizedButton;
